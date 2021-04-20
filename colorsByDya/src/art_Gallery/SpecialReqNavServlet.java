c83d7a4074019b82dba70523eca597126e44159a
witchx44 <witchx44@gmail.com>
2021-04-20 10:22:58 +0530

    Update

--- colorsByDya/src/art_Gallery/SpecialReqNavServlet.java
+++ colorsByDya/src/art_Gallery/SpecialReqNavServlet.java
@@ -13,5 +13,8 @@ public class SpecialReqNavServlet extends HttpServlet {
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		// Gets session
+		// HttpSession 
 	}
 }
