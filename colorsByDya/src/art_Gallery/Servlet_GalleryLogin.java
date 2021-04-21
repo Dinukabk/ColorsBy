e1368238a9c8e10bf55cce51f47d4ae479b9e841
witchx44 <witchx44@gmail.com>
2021-04-22 00:01:00 +0530

    Merged Achini's

--- colorsByDya/src/art_Gallery/Servlet_GalleryLogin.java
+++ colorsByDya/src/art_Gallery/Servlet_GalleryLogin.java
@@ -9,49 +9,34 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 @WebServlet("/Servlet_GalleryLogin")
 public class Servlet_GalleryLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(false);
 		
-		//PrintWriter out = response.getWriter();
-		//response.setContentType("text/html");
-		
-		String username = request.getParameter("username");
-		String password = request.getParameter("password");
-		
-		/*
-		 * boolean isTrue;
-		 * 
-		 * isTrue = PaintingDBUtil.validate(username, password);
-		 * 
-		 * if (isTrue == true) { List<Painting> paintingDetails =
-		 * PaintingDBUtil.displayPaintingList(username, password);
-		 * 
-		 * request.setAttribute("paintingDetails", paintingDetails);
-		 * 
-		 * RequestDispatcher dispatch = request.getRequestDispatcher("Artworks.jsp");
-		 * 
-		 * dispatch.forward(request, response); } else {
-		 * out.println("<script type='text/javascript'>");
-		 * out.println("alert('Your username or password is incorrect');");
-		 * out.println("location='login.jsp'"); out.println("</script>"); }
-		 */
-		
-		try {
-			List<Painting> paintingDetails = PaintingDBUtil.displayPaintingList(username, password);
-			request.setAttribute("paintingDetails", paintingDetails);
-		}
-		catch(Exception e){
-			e.printStackTrace();
+		if (session != null) {
+			int ArtistUserID = (int) session.getAttribute("artistUserID");
+			System.out.println("Artist ID in servlet: " + ArtistUserID);
+			try {
+				List<Painting> paintingDetails = PaintingDBUtil.displayPaintingList(ArtistUserID);
+				request.setAttribute("paintingDetails", paintingDetails);
+				RequestDispatcher dispatch = request.getRequestDispatcher("Artworks.jsp");
+				dispatch.forward(request, response);
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+		} else {
+			RequestDispatcher RD = request.getRequestDispatcher("artistLogin.jsp");
+			RD.forward(request, response);
 		}
 		
-		RequestDispatcher dispatch = request.getRequestDispatcher("Artworks.jsp");
-		dispatch.forward(request, response);
+		
 		
 	}
 
