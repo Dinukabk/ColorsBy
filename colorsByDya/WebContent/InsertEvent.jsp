6b1e6fd266d05821abbd0a4e2c1263578e7f52b6
Devvish007 <devindavishva@gmail.com>
2021-05-12 23:43:13 +0530

    made event class connection = EventDBconnection

--- colorsByDya/WebContent/InsertEvent.jsp
+++ colorsByDya/WebContent/InsertEvent.jsp
@@ -16,6 +16,7 @@
 	
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   	<link rel="stylesheet" href="/resources/demos/style.css">
+  	
   	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
   	<script>
@@ -68,7 +69,7 @@
 	-->
 	
 	
-	 <form action="insertEve" method="post">
+	 <form action="insertEvent" method="post">
 	 
 	 	<input type="text" name="name" placeholder="Enter event name" ><br>
 	 	
--- colorsByDya/src/art_Gallery/eventDBUtil.java
+++ colorsByDya/src/art_Gallery/eventDBUtil.java
@@ -2,30 +2,27 @@ package art_Gallery;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class eventDBUtil {
+	
+	private static Connection con = null ;
+	private static Statement stmt = null ;
+	private static ResultSet rs = null;
+	
 	
 	public static boolean insertEvent(String name, String date, String status, String description, String adminID ) {
 		
 		boolean isSuccess = false;
 		
-		//create database connection
-		String url = "jdbc:mysql://localhost:3306/colorbydiyaa";
-		String username = "root";
-		String password = "root";
-		
-		
 		try 
 		{
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection(url, username, password);
-			Statement stmt = con.createStatement();
+			con = EventDBconnect.getConnection();
+			stmt = con.createStatement();
 			
 			boolean stats = Boolean.parseBoolean(status);
 			int admin = Integer.parseInt(adminID);
-			
-			
 			String sql = "insert into event (0,  '"+name+"', '"+date+"', '"+stats+"', '"+description+"', '"+admin+"')";  
 			
 			int result = stmt.executeUpdate(sql);
